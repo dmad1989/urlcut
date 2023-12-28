0054b9fe2e3d42ed093f81e1737d163856997a48
dmad1989 <dmad1989@gmail.com>
2023-12-28 16:27:42 +0300

    feat add redirect handler, bugfix

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -27,20 +27,17 @@ func Manage(res http.ResponseWriter, req *http.Request) {
 }
 
 func cutter(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("I m in cutter")
 	if req.Method != http.MethodPost {
 		res.WriteHeader(http.StatusBadRequest)
+		return
 	}
-
 	body, err := io.ReadAll(req.Body)
-	fmt.Println("I m in cutter- 2, ", req.URL)
 	if err != nil {
 		res.WriteHeader(http.StatusBadRequest)
 		res.Write([]byte(err.Error()))
 		return
 	}
 	res.Header().Set("Content-Type", "text/plain")
-	// res.Header().Set("Content-Length", "30")
 	res.WriteHeader(http.StatusCreated)
 	res.Write([]byte(fmt.Sprintf("http://%s%s%s\n", req.Host, req.URL.Path, doCut(body))))
 }
@@ -59,27 +56,15 @@ func doCut(body []byte) (generated string) {
 func redirect(res http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodGet {
 		res.WriteHeader(http.StatusBadRequest)
+		return
 	}
-	fmt.Println("I m in redirect- 1")
-	// Эндпоинт с методом GET и путём /{id}, где id — идентификатор сокращённого URL (например, /EwHXdJfB). В случае успешной обработки запроса сервер возвращает ответ с кодом 307 и оригинальным URL в HTTP-заголовке Location.
-	// Пример запроса к серверу:
-	// GET /EwHXdJfB HTTP/1.1
-	// Host: localhost:8080
-	// Content-Type: text/plain
-	// Пример ответа от сервера:
-	// HTTP/1.1 307 Temporary Redirect
-	// Location: https://practicum.yandex.ru/
-
-	//TODO
-	// проверка входящего параметра
-	// поиск в сохраненных значениях URL для редиректа
-	v := urlMap.getKey("fd")
-	//построение ответа от сервиса
-	res.Header().Set("content-type", "application/json")
-	// устанавливаем код 200
-	res.WriteHeader(http.StatusOK)
-	// пишем тело ответа
-	res.Write([]byte("HI"))
+	path := req.URL.Path[1:]
+	redirectUrl := urls.getKey(path)
+	if redirectUrl == "" {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	http.Redirect(res, req, redirectUrl, http.StatusTemporaryRedirect)
 }
 
 type urlMap map[string]string
@@ -101,14 +86,14 @@ func (u urlMap) has(key string) bool {
 	return ok
 }
 
-func (u urlMap) getKey(value string) string {
+func (u urlMap) getKey(value string) (res string) {
 	if len(u) == 0 {
-		return ""
+		return
 	}
 	for key, val := range u {
 		if val == value {
 			return key
 		}
 	}
-
+	return
 }
