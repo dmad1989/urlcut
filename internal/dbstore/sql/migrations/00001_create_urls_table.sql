fe556bbd801507820021bc3d6dffa98f613276d5
dmad1989 <dmad1989@gmail.com>
2024-02-25 11:12:15 +0300

    feat refactor table creation

--- internal/dbstore/sql/migrations/00001_create_urls_table.sql
+++ internal/dbstore/sql/migrations/00001_create_urls_table.sql
@@ -1,27 +1,36 @@
 -- +goose Up
 -- +goose StatementBegin
-create table if not exists public.urls
-	(
-		"ID" bigint not null generated always as identity ( increment 1 start 1 minvalue 1 maxvalue 1000000 cache 1 ),
-		short_url text collate pg_catalog."default" not null,
-		original_url text collate pg_catalog."default",
-		constraint urls_pkey primary key ("ID")
-	)	
-	tablespace pg_default;
+CREATE TABLE IF NOT EXISTS public.urls
+(
+    "ID" bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 1000000 CACHE 1 ),
+    short_url text COLLATE pg_catalog."default" NOT NULL,
+    original_url text COLLATE pg_catalog."default",
+    "authorId" text COLLATE pg_catalog."default" NOT NULL,
+    deletedflag boolean NOT NULL DEFAULT false,
+    CONSTRAINT urls_pkey PRIMARY KEY ("ID"),
+    CONSTRAINT urls_original_unique UNIQUE (original_url)
+)
 
-alter table if exists public.urls owner to postgres;
+TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.urls
-    ADD COLUMN "authorId" text NOT NULL;
+    OWNER to postgres;
+-- Index: original_url
 
-create index short_url on
-urls (short_url);
+-- DROP INDEX IF EXISTS public.original_url;
 
-create index original_url on
-urls (original_url);
+CREATE INDEX IF NOT EXISTS original_url
+    ON public.urls USING btree
+    (original_url COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: short_url
 
-alter table public.urls add constraint urls_original_unique unique (original_url);
+-- DROP INDEX IF EXISTS public.short_url;
 
+CREATE INDEX IF NOT EXISTS short_url
+    ON public.urls USING btree
+    (short_url COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
 -- +goose StatementEnd
 
 -- +goose Down
